5d4f40aa52febbb4885b59d60bf7c5b88711ff71
brian <brian.qi138@gmail.com>
2021-09-11 10:24:11 -0600

    merge from LoggedActivities: updated views and url.py

--- WellnessCompanion/WellnessApplication/urls.py
+++ WellnessCompanion/WellnessApplication/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mainpage, name='mainpage'),
+    path('companion', views.companionpage, name='companionpage'),
+    path('submit', views.submitpage, name='submitpage'),
+    path('logs', views.logpage, name='logpage'),
 ]
