6555868567ba6e341a7c75d2326a92de0e306258
aschafer1 <aschafer@ualberta.ca>
2021-09-11 13:54:55 -0600

    companion webpage now shows the category you have done the least of

--- WellnessCompanion/WellnessApplication/views.py
+++ WellnessCompanion/WellnessApplication/views.py
@@ -4,9 +4,16 @@ from django.http import HttpResponse
 from .models import Activity
 # Create your views here.
 def companionpage(request):
-    latest_activity_category = Activity.objects.order_by('-date')[:5]
-    #output = latest_activity_category#.activity_catergory
-    output = ', '.join([q.activity_catergory for q in latest_activity_category])
+    latest_activity_category = Activity.objects.order_by('-date')[:50]
+    count = {}
+    for i in latest_activity_category:
+        if i.activity_catergory not in count:
+            count[i.activity_catergory] = 1
+        else:
+            count[i.activity_catergory] += 1
+
+
+    output = sorted(count)[0]
     return HttpResponse(output)
 def submitpage(request):
     return HttpResponse("This is the submit page")
