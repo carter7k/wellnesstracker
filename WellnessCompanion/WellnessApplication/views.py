4c18b7dbd0af3b5f104b7fedb4743dd41175d1e6
aschafer1 <aschafer@ualberta.ca>
2021-09-11 13:25:39 -0600

    companion page now lists recently done categories

--- WellnessCompanion/WellnessApplication/views.py
+++ WellnessCompanion/WellnessApplication/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Activity
 # Create your views here.
 def companionpage(request):
-    return HttpResponse("This is the companion page")
+    latest_activity_category = Activity.objects.order_by('-date')[:5]
+    #output = latest_activity_category#.activity_catergory
+    output = ', '.join([q.activity_catergory for q in latest_activity_category])
+    return HttpResponse(output)
 def submitpage(request):
     return HttpResponse("This is the submit page")
 def logpage(request):
