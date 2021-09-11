3b040fd2a091e71cebb972a36451c17a6419cc0a
brian <brian.qi138@gmail.com>
2021-09-11 09:44:45 -0600

    instantiated classes Activity and User

--- WellnessCompanion/WellnessApplication/models.py
+++ WellnessCompanion/WellnessApplication/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+import uuid
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class Activity(models.Model):
+    date = models.DateTimeField("publish date")
+    activity_type = "placeholder activity"
+    personID = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.activity_type
